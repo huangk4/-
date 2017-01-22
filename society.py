8b300024f08ac88ed499315165b31aa0ebce41c8
huangk <847209149@qq.com>
2017-01-22 21:19:33 +0800

    Signed-off-by: huangk <847209149@qq.com>

--- society.py
+++ society.py
@@ -1,9 +1,18 @@
 from pymongo import MongoClient
 
-client = MongoClient('192.168.65.137', 27017)
-db_name = 'db'
-db = client[db_name]
-con_action= db['action']
+
+
+#这里写宏
+g_server_ip='192.168.65.137'
+g_server_port=27017
+g_db_name='test'  #库名表名先用自己的
+g_tb_name='table_one'
+
+##########
+
+
+client = MongoClient(server_ip,server_port)
+db = client[g_db_name]
 print(db.post)
 
 
