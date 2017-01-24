691fec5882343beebedd2068ed1566ce2f4cd1a2
lzj <leezj9671@gmail.com>
2017-01-24 20:50:26 +0800

    test

--- society.py
+++ society.py
@@ -1,10 +1,11 @@
 #coding:utf-8
 from pymongo import MongoClient
-from flask import Flask
+from flask import Flask, request, session, redirect, \
+    render_template, url_for, flash
 
 
 #这里写宏
-g_server_ip='192.168.65.137'
+g_server_ip='127.0.0.1'
 g_server_port=27017
 g_db_name='test'  #库名表名先用自己的
 g_tb_name='table_one'
@@ -16,59 +17,76 @@ client = MongoClient(g_server_ip,g_server_port)
 db = client[g_db_name]
 #-----------------
 
-
-
 app = Flask(__name__)
 
+#默认配置
+app.config.update(dict(
+    DEBUG = True,
+    SECRET_KEY = 'development',
+    USERNAME = 'admin',
+    PASSWORD = 'adadad'
+))
+
 @app.route('/')
-def web_show(param,word): #这里不知道怎么传参数，能解决的就帮忙解决一下
-    show='<center>'
-    try:
-        results=db.person.find({param:word})
-        for result in results:            
-            show=show+str(result)
-            show=show+'<p>'
-    except:
-        return show
-    show=show+'</center>'
-    return show
-    
+def main_redirect():
+    return redirect(url_for('login'))
+        
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''登录'''
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show'))
+    return render_template('login.html', error=error)
 
-#正则表达式，用来将查询结果的字段和值分开显示
-def show_column():
-    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show'))
 
+@app.route('/show')
+def show():
+    #待实现
+    return render_template('showdb.html')
 
-
-
+@app.route('/add', methods=['POST'])
+def add_document():
+    #添加，待实现
+    return redirect(url_for('show'))
 
 if __name__=='__main__':
     app.run()
     
 
 
-#查询函数，param为查询字段，word为查询的值
-def search(param,word):
-    try:
-        results=db.person.find({param:word})
-        for result in results:
-            print(result)
-    except:
-        print('没有结果')
+# @app.route('/')
+# def web_show(param,word): #这里不知道怎么传参数，能解决的就帮忙解决一下
+#     show='<center>'
+#     try:
+#         results=db.person.find({param:word})
+#         for result in results:            
+#             show=show+str(result)
+#             show=show+'<p>'
+#     except:
+#         return show
+#     show=show+'</center>'
+#     return show
+# # #查询函数，param为查询字段，word为查询的值
+# def search(param,word):
+#     try:
+#         results=db.person.find({param:word})
+#         for result in results:
+#             print(result)
+#     except:
+#         print('没有结果')
 
-'''
-#删除表
-db.person.drop() 
-
-#网数据库集合中添加数据
-db.person.save({'name':'ak2','email':'5542323478@qq.com',\
-                'password':'admin','passwordHash':'E45345HGFGRRRDFFGHSDHF3Y98HFG49H'})
-
-#查询数据库集合中所有数据
-results=db.person.find()
-for result in results:
-    print(result)
-print(db.post)
-'''
-
-
+# #正则表达式，用来将查询结果的字段和值分开显示
+# def show_column():
