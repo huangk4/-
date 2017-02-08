eb39ad72ea365daee72477a0cfcad5b2e17f16b8
huangk <847209149@qq.com>
2017-02-08 22:46:21 +0800

    Signed-off-by: huangk <847209149@qq.com>

--- society.py
+++ society.py
@@ -10,7 +10,7 @@ import re
 
 
 #这里写宏和配置信息
-g_server_ip='127.0.0.1'
+g_server_ip='192.168.65.137'
 g_server_port=27017
 g_db_name='test'  #库名表名先用自己的
 g_tb_name='table_one'
@@ -121,6 +121,8 @@ def upload():
             #读取文件转换格式插入数据库
             fp=open(path,'r')
             for line in fp:
+                if line=='\n':
+                    continue
                 linedata={}
                 line=line.strip('\n')
                 group=line.split(fenge)    
@@ -241,16 +243,58 @@ def analysis_email():
     for i in count:
         counts+=count[i]
     for i in count:
-        emails[i]=str(((count[i]/counts)*100)%101)
+        emails[i]=(str(round(((count[i]/counts)*100)%101,1))+'%')
     print(emails)
     return emails
-        
+
+#来源分析函数,查询数据库返回所有来源及所占比的字典
+def analysis_source():
+    wei=[]
+    count={}
+    results= db.person.find({},{"source":1,"_id":0})
+    for result in results:
+        if 'source' in result:
+            source=result['source']
+            if not source in wei:
+                wei.append(source)
+                count[source]=1
+            else:
+                count[source]+=1
+    counts=0
+    sources={}
+    for i in count:
+        counts+=count[i]
+    for i in count:
+        sources[i]=(str(round(((count[i]/counts)*100)%101,1))+'%')
+    return sources
+
+#泄露时间分析函数,查询数据库返回所有泄露时间及所占比的字典
+def analysis_xtime():
+    wei=[]
+    count={}
+    results= db.person.find({},{"xtime":1,"_id":0})
+    for result in results:
+        if 'xtime' in result:
+            xtime=result['xtime']
+            if not xtime in wei:
+                wei.append(xtime)
+                count[xtime]=1
+            else:
+                count[xtime]+=1
+    counts=0
+    xtimes={}
+    for i in count:
+        counts+=count[i]
+    for i in count:
+        xtimes[i]=(str(round(((count[i]/counts)*100)%101,1))+'%')
+    return xtimes
 
 if __name__=='__main__':
     
     #file_insert('wxsuv.txt')
     #search('name','ak')
     #app.run()
-    analysis_email()
+    print(analysis_source())
+    
     
 
